73192c5e13bcb6d952abe738594c0a141a5209e6
ms <shaheed_manzur@yahoo.com>
2021-04-28 13:20:41 -0400

    formatting and screen refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,9 +18,9 @@ function addHours() {
         // <div class="col-md-1 hour">
         //     9AM
         // </div>
-        // <textarea class="col-md-10 description">
+        // <textarea class="col-md-10" id="txt-9">
         // </textarea>
-        // <button class="btn saveBtn col-md-1"><i class="fas fa-save"></i></button>
+        // <button class="btn saveBtn col-md-1 btn-outline-secondary"></button>
         // </div>
 
         // create tags per above and add to row
@@ -44,6 +44,7 @@ function addHours() {
 function loadSavedSchedule() {
     var hr, txtAreaID;
 
+    // load each hours data from local storage
     for (hr = 9; hr < 18; hr++) {
         txtAreaID = "txt-" + hr.toString();
         $('textarea#'+txtAreaID).val(localStorage.getItem(txtAreaID));
@@ -52,8 +53,29 @@ function loadSavedSchedule() {
 
 // update hour's color
 function updateHourColor() {
+    // get current hour
+    var currentHr = moment().hours();
+    // console.log(currentHr);
 
-
+    // check against each hour block for past, present, future
+    for (hr = 9; hr < 18; hr++) {
+        txtAreaID = "txt-" + hr.toString();
+        // past 
+        if (hr < currentHr) {
+            $('textarea#'+txtAreaID).addClass("past");
+        }
+        // present
+        else if (hr === currentHr) {
+            $('textarea#'+txtAreaID).removeClass("past");
+            $('textarea#'+txtAreaID).addClass("present");
+        }
+        // future
+        else {
+            $('textarea#'+txtAreaID).removeClass("past");
+            $('textarea#'+txtAreaID).removeClass("present");
+            $('textarea#'+txtAreaID).addClass("future");
+        }        
+    }
 }
 
 // save schedule
@@ -77,13 +99,23 @@ function saveSchedule() {
 
 // function docHandler
 function docHandler() {
+    // display today's date
     displayDate();
+
+    // load previously saved schedule
     loadSavedSchedule();
+
+    // timeblocks are color coded
     updateHourColor();
+
+    // event handler for save button
     $('.saveBtn').on('click', saveSchedule);
+
+    // set timer to update timeblocks color in every 10sec
+    var interval = setInterval(updateHourColor, 10000);
 }
 
-// invoke addHours on window load
+// invoke addHours on window load to create hours in hr-container
 $(window).on('load', addHours);
 
 // once document is ready, update page and add event handler
